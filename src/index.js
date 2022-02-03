1f034bf147de4954aee023aea7f2f334d798d4e1
MawkaA <annushka772@gmail.com>
2022-02-03 01:06:17 -0800

    1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import './sass/main.scss';
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[menu-open]"),
-    closeMenuBtn: document.querySelector("[menu-close]"),
-    menu: document.querySelector("[menu]"),
-  };
-
-  refs.openMenulBtn.addEventListener("click", toggleMenu);
-  refs.closeMenulBtn.addEventListener("click", toggleMenu);
-
-  function toggleModal() {
-    refs.menu.classList.toggle("is-hidden");
-  }
-})();
